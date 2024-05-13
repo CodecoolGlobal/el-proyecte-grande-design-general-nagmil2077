d37a4a4cf74cfe7a2a6fdc72242c6df833be6ad4
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-05-13 16:37:35 +0200

    Remove unnecessary and unused code from RegisterPage.jsx

--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -1,4 +1,3 @@
-import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import SideBar from "../Components";
 import {Box, Button, Container, TextField, Typography} from "@mui/material";
