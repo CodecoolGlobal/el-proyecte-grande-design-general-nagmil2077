c5e480458b709240fb382d26b5512ca1006d0719
nagmil2077 <68022252+nagmil2077@users.noreply.github.com>
2024-05-13 13:34:56 +0200

    Remove form and modify fetch method in RegisterPage.jsx

--- frontend/src/Pages/RegisterPage.jsx
+++ frontend/src/Pages/RegisterPage.jsx
@@ -2,41 +2,28 @@ import React, {useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import SideBar from "../Components";
 import {Box, Button, Container, TextField, Typography} from "@mui/material";
+import UserForm from "../Components/UserForm";
+
+const registerUser = (formData) => {
+    return fetch('/api/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    }).then((res) => res.json());
+}
 
 const Register = () => {
     const navigate = useNavigate()
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-    });
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
+    const handleSubmit = (formData) => {
         try {
-            const response = await fetch('/api/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-
-            if (response.ok) {
-                navigate('/');
-            } else {
-                console.error('Registration failed');
-            }
+            registerUser(formData)
+                .then(() => {
+                    navigate('/');
+                });
         } catch (error) {
             console.error('Registration error:', error);
         }
@@ -48,52 +35,10 @@ const Register = () => {
             <Typography variant="h4" component="h2" sx={{mt: 3, mb: 2}}>
                 Registration
             </Typography>
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': {m: 1, width: '25ch'},
-                }}
-                noValidate
-                autoComplete="off"
-                onSubmit={handleSubmit}
-            >
-                <div>
-                    <TextField
-                        label="Username"
-                        type="text"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                        variant="outlined"
-                    />
-                </div>
-                <div>
-                    <TextField
-                        label="Email"
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                        variant="outlined"
-                    />
-                </div>
-                <div>
-                    <TextField
-                        label="Password"
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        required
-                        variant="outlined"
-                    />
-                </div>
-                <Button type="submit" variant="contained" sx={{mt: 3}}>
-                    Register
-                </Button>
-            </Box>
+            <UserForm
+                onSave={handleSubmit}
+                onCancel={() => navigate(`/`)}
+            />
         </Container>
     );
 };
